33e821c3845a2f151a3aa65ca6a281cb6756fd7a
Stephanie Resis <sresis@umich.edu>
2020-07-28 11:04:39 -0700

    added melon list

---
+++ melonCounting.js
@@ -0,0 +1,13 @@
+const melonsToAdd = ['Ogen', 'Horned Melon', 'Watermelon', 'Casaba',
+                 'Sharlyn', 'Xigua', 'Ogen', 'Christmas', 'Christmas',
+                 'Christmas', 'Christmas', 'Watermelon', 'Sharlyn', 'Xigua',
+                 'Cantaloupe', 'Christmas', 'Watermelon', 'Christmas',
+                 'Sharlyn', 'Christmas', 'Cantaloupe', 'Casaba', 'Cantaloupe',
+                 'Santa Claus', 'Horned Melon', 'Watermelon', 'Ogen',
+                 'Horned Melon', 'Cantaloupe', 'Xigua', 'Horned Melon', 'Sharlyn',
+                 'Horned Melon', 'Sharlyn', 'Cantaloupe', 'Christmas',
+                 'Horned Melon', 'Horned Melon', 'Horned Melon', 'Xigua', 'Xigua',
+                 'Watermelon', 'Cantaloupe', 'Casaba', 'Cantaloupe', 'Casaba',
+                 'Watermelon', 'Santa Claus', 'Casaba'];
+
+function countMelons(melonList):
